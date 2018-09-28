c09fe486070e9f471d116bb434e221cc3f789df8
shubham mahore <shubhammahore@gmail.com>
2018-09-28 12:53:05 +0530

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Users } from '../users/users';
-//import { NavController } from 'ionic-angular';
-//import { Users } from '../users/users';
+import { NavController } from 'ionic-angular';
+
 //import { ShopPage } from '../shop/shop';
 //import { BuyoutPage } from '../buyout/buyout';
 
@@ -11,16 +11,17 @@ import { Users } from '../users/users';
 })
 export class HomePage {
 user = Users;
-
-
+constructor(public navCtrl: NavController) {
+  //console.log('hello');
+}
+onGoToUsers(){
+  this.navCtrl.push(this.user)
+  .catch((error) => console.log('Access Denied , Argument was '+ error));
+  //this.navCtrl.setRoot(Users);
+}
   /*
-  constructor(public navCtrl: NavController) {
-    //console.log('hello');
-  }
-  onGoToUsers(){
-    this.navCtrl.push(Users);
-    //this.navCtrl.setRoot(Users);
-  }
+ 
+  
   onGoToShop(){
     this.navCtrl.push(ShopPage)
   }
